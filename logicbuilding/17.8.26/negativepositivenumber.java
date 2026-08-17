efa2e00e7f01dd107b68b36430b19fcd597d3762
divyashishnegicmaug26 <divyashish.negi.cmaug26@gmail.com>
2026-08-17 18:37:13 +0530

    Add files via upload

---
+++ logicbuilding/17.8.26/negativepositivenumber.java
@@ -0,0 +1,13 @@
+public class negativepositivenumber {
+    public static void main(String[] args) {
+        int n = 0;
+        if (n > 0) {
+            System.out.println("even number " + n);
+
+        } else {
+            System.out.println("odd number " + n);
+        }
+
+    }
+
+}
---
+++ logicbuilding/17.8.26/rectanglearea.java
@@ -0,0 +1,8 @@
+public class rectanglearea {
+    public static void main(String[] args) {
+        int length = 10;
+        int breadth = 20;
+        System.out.println("area is" + length * breadth);
+    }
+
+}
