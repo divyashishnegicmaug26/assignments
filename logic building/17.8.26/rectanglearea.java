e98caf291f08cb5bdffe38514343d074d387fe95
divyashishnegicmaug26 <divyashish.negi.cmaug26@gmail.com>
2026-08-17 18:35:18 +0530

    Delete logic building/17.8.26 directory

--- logic building/17.8.26/rectanglearea.java
+++
@@ -1,8 +0,0 @@
-public class rectanglearea {
-    public static void main(String[] args) {
-        int length = 10;
-        int breadth = 20;
-        System.out.println("area is" + length * breadth);
-    }
-
-}
--- logic building/17.8.26/squareareq.java
+++
@@ -1,6 +0,0 @@
-public class squareareq {
-    public static void main(String[] args) {
-        int side = 5;
-        System.out.println("side is " + side * side);
-    }
-}
